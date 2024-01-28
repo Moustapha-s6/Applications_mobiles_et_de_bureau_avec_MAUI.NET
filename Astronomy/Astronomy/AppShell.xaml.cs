c8edf36ebf507471d25a21bbe2adc30d16544860
Moustapha-s6 <msy43467@gmail.com>
2024-01-28 16:21:47 +0000

    initial commit

---
+++ Astronomy/Astronomy/AppShell.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Astronomy
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+
+           
+        }
+    }
+}
---
+++ Astronomy/Astronomy/Pages/AstroName.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Astronomy.Pages
+{
+    internal class AstroName
+    {
+        private string astroName;
+
+        public string Name
+        {
+            get => astroName;
+            set
+            {
+                astroName = value;
+
+                // Appeler la méthode pour mettre à jour l'interface utilisateur si nécessaire
+                 UpdateAstroBodyUI(astroName);
+            }
+        }
+
+        // Si vous avez une méthode pour mettre à jour l'interface utilisateur, définissez-la ici
+         private void UpdateAstroBodyUI(string name)
+         {
+             // Code pour mettre à jour l'interface utilisateur
+         }
+    }
+}
